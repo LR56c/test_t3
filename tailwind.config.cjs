c77f19c496da6d1867da00004a7eed836983d8dd
Mauricio Herrera Ortega <ronalli56@gmail.com>
2022-09-14 22:57:33 -0300

    test daisyui & flowbite w/tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    // "./node_modules/flowbite/**/*.js",
+    // 'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+    // "./pages/**/*.{ts,tsx}",
+    // "./public/**/*.html",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require("daisyui"),
+    // require('flowbite/plugin')
+  ],
 };
